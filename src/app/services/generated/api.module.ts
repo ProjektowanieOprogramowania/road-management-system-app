2be4afd0c513bd661b1f1aef15925ff9c2fc271c
kacper <kacper.tarlowski20@gmail.com>
2022-11-09 01:23:26 +0100

    add generated api

---
+++ src/app/services/generated/api.module.ts
@@ -0,0 +1,35 @@
+import { NgModule, ModuleWithProviders, SkipSelf, Optional } from '@angular/core';
+import { Configuration } from './configuration';
+import { HttpClient } from '@angular/common/http';
+
+import { ChargesService } from './api/charges.service';
+import { PassingChargesService } from './api/passingCharges.service';
+import { PenaltiesService } from './api/penalties.service';
+import { SubscriptionsService } from './api/subscriptions.service';
+import { TariffsService } from './api/tariffs.service';
+
+@NgModule({
+  imports:      [],
+  declarations: [],
+  exports:      [],
+  providers: []
+})
+export class ApiModule {
+    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders<ApiModule> {
+        return {
+            ngModule: ApiModule,
+            providers: [ { provide: Configuration, useFactory: configurationFactory } ]
+        };
+    }
+
+    constructor( @Optional() @SkipSelf() parentModule: ApiModule,
+                 @Optional() http: HttpClient) {
+        if (parentModule) {
+            throw new Error('ApiModule is already loaded. Import in your base AppModule only.');
+        }
+        if (!http) {
+            throw new Error('You need to import the HttpClientModule in your AppModule! \n' +
+            'See also https://github.com/angular/angular/issues/20575');
+        }
+    }
+}
---
+++ src/app/services/generated/api/api.ts
@@ -0,0 +1,11 @@
+export * from './charges.service';
+import { ChargesService } from './charges.service';
+export * from './passingCharges.service';
+import { PassingChargesService } from './passingCharges.service';
+export * from './penalties.service';
+import { PenaltiesService } from './penalties.service';
+export * from './subscriptions.service';
+import { SubscriptionsService } from './subscriptions.service';
+export * from './tariffs.service';
+import { TariffsService } from './tariffs.service';
+export const APIS = [ChargesService, PassingChargesService, PenaltiesService, SubscriptionsService, TariffsService];
