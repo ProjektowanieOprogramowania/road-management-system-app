0a4f3574b914fdb9887a9d50ca0664fff75c4c8c
Norsien <smola27ms@gmail.com>
2022-11-24 00:04:20 +0100

    vovoideshieppe

--- src/app/pages/sensors/sensor-register-form/sensor-register-form.component.ts
+++ src/app/pages/sensors/sensor-register-form/sensor-register-form.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import {Sensor, SensorsService} from "../../../services/generated";
 
 @Component({
   selector: 'app-sensor-register-form',
@@ -7,41 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SensorRegisterFormComponent implements OnInit {
 
-//   export interface Sensor {
-//     id?: number;
-//     name: string;                    done
-//     localization: Localization;
-//     voivodeship: Voivodeship;
-//     serialNumber: string;            done
-//     sensorType: SensorType;          ok
-//     enabled: boolean;                done
-//     webhookUrl?: string;
-// }
-
   name: string = "";
   serialNumber: string = "";
   selectedSensorType: string = ""
   active: boolean = true;
-  webhookUrl: string = "";
-  selectedOption: any;
+  localizationString: string = ""
 
   sensorNameValid: boolean = true;
   serialNumberValid: boolean = true;
   sensorTypes: string[]
 
-  selectedOption: any;
+  selectedOptions: any;
+  selectedPosition: any;
+  selectedLocation: any[] = [];
+  isAddingNodesModeOn: boolean = false;
+  disabled: boolean = true;
 
-  constructor() {
-    this.sensorTypes = [
-      "A", "B", "C"
-    ]
+  constructor(private sensorService: SensorsService, private http: HttpClient) {
+    this.sensorTypes = ["A", "B"]
   }
 
   ngOnInit(): void {
-    this.selectedOption = {
+    this.selectedOptions = {
       center: new google.maps.LatLng(52.1645673, 19.9203192),
       zoom: 7.3
     }
+  }
+
+  setAddingNodesMode() {
+    this.isAddingNodesModeOn = true   
+  }
+
+  handleMapClick(event: any) {
+    if (!this.isAddingNodesModeOn) {
+      return;
+    }
+    this.selectedPosition = event.latLng;
+    this.addMarker()
+  }
+
+  addMarker() {
+    this.selectedLocation = [(new google.maps.Marker({
+      position: {
+        lat: this.selectedPosition.lat(),
+        lng: this.selectedPosition.lng()
+      },
+    }))];
+    this.isAddingNodesModeOn = false;
+    this.getVoivodeship(this.selectedLocation[0].position.lat(), this.selectedLocation[0].position.lng())
+    this.removePolish()
   }
 
   validateForm() {
@@ -50,6 +67,46 @@ export class SensorRegisterFormComponent implements OnInit {
     } else {
       this.sensorNameValid = true;
     }
+  }
+
+  getReverseGeoCode(lat: number, lon: number): Observable<any> {
+    return this.http.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&sensor=false&key=AIzaSyDU3n7u3bjvvzAZBvZgGuGv9L_872q4fHo&language=pl-PL")
+    }
+    
+  getVoivodeship(lat: number, lon: number) {
+  this.getReverseGeoCode(lat, lon)
+    .subscribe(
+    products => {
+      this.localizationString = products.results[Object.keys(products.results).length - 2].formatted_address.split(",")[0];
+    });
+  }
+
+  removePolish(){
+    const letters     = ['ą', 'ć', 'ę', 'ł', 'ń', 'ó', 'ś', 'ź', 'ż'];
+    const replacement = ['a', 'c', 'e', 'l', 'n', 'o', 's', 'z', 'z'];
+
+    let result = this.localizationString;
+    console.log(this.localizationString)
+    for (let i = 0; i < letters.length; ++i) {
+        result = result.replace(letters[i], replacement[i]).replace("-","");
+    }
+    
+    this.localizationString = result
+  }
+
+  submit(){
+    // const sensor: Sensor = {
+    //   name: this.name,
+    //   localization: this.selectedLocation[0].position,
+    //   voivodeship: "",
+    //   serialNumber: this.serialNumber,
+    //   sensorType: this.selectedSensorType,
+    //   enabled: this.active,
+    // }
+    // this.validateForm()
+    // if(this.sensorNameValid&&this.serialNumberValid&&this.selectedLocation.length === 1) {
+
+    // }
   }
 
 }
