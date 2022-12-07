71aece604bb630968382c69ed8915b58353c0b8c
Bartlomiej Kopysc <bkopysc@wp.pl>
2022-12-07 12:55:51 +0100

    Probably working offer score submitting

--- src/app/pages/offers/offers-page/offers-page.component.ts
+++ src/app/pages/offers/offers-page/offers-page.component.ts
@@ -23,20 +23,7 @@ export class OffersPageComponent implements OnInit {
 
   rating = 0;
 
-  auctionOfferList: AuctionOffer[] = [
-    {
-      id: 2,
-      userId: 'lalala',
-      amount: 123,
-      currency: 'PLN'
-    },
-    {
-      id:3,
-      userId: 'lalal',
-      amount: 300,
-      currency: 'EUR'
-    }
-  ];
+  auctionOfferList: AuctionOffer[] = [];
 
   subscription = new Subscription();
 
@@ -54,10 +41,11 @@ export class OffersPageComponent implements OnInit {
       this.subscription.add(this.auctionService.getAuctionById(this.auctionId).subscribe({
         next: value => {
           this.auction = value;
-          this.getOffers();
+          this.getOffers(value.id!);
         },
         error: err => {
-          this.isLoading = false;
+          this.isLoading = false; //TODO: usunac
+          this.auctionOfferList = this.mockOffers; //TODO: usunac
           this.messageService.add({severity: 'error', summary: 'Server Error!', detail: 'Error while getting Auction'});
         }}))
     }
@@ -65,10 +53,19 @@ export class OffersPageComponent implements OnInit {
   }
 
   //branie ofert dla danego przetargu
-  private getOffers(){
+  private getOffers(auctionId: number){
     //this.isLoading = true;
     //this.subscription.add(this.auctionOfferService.) bra
     //this.auctionOfferService.getAllOffers()
+    this.subscription.add(this.auctionOfferService.getAuctionOffers(auctionId)
+      .subscribe({
+        next:value => {
+          this.auctionOfferList = value;
+          this.isLoading = false;
+        },
+        error: err => {
+          this.messageService.add({severity: 'error', summary: 'Server Error!', detail: 'Error while getting Offers'});
+        }}));
   }
 
   ngOnInit(): void {
@@ -89,10 +86,60 @@ export class OffersPageComponent implements OnInit {
   onOfferSubmit() {
     this.ratingLoading = true;
 
-    //przesylanie opinii
+    const newOffer = Object.assign({}, this.selectedOffer);
+    newOffer.score = this.rating;
+
+    this.subscription.add(this.auctionOfferService.updateOfferScore(newOffer)
+      .subscribe({
+        next: value => {
+          const index = this.auctionOfferList.findIndex(o => o.id == value.id);
+          if(!index){
+            this.messageService.add({severity: 'error', summary: 'Server Error!', detail: 'Error while submitting score [id]'});
+            return;
+          }
+          this.auctionOfferList[index] = value;
+          this.ratingLoading = false;
+          this.detailsModalVisiblle = false;
+          this.messageService.add({severity: 'success', summary: 'Sukces!', detail: 'Poprawnie wystawiono ocenę!'});
+        },
+        error: err => {
+          this.messageService.add({severity: 'error', summary: 'Server Error!', detail: 'Error while submitting score'});
+          this.ratingLoading = false;
+          this.detailsModalVisiblle = false;
+        }}));
   }
 
   backToAuctions() {
     this.router.navigate(['auctions']);
+  }
+
+   mockOffers = [
+    {
+      id: 1,
+      userId: 'abc123',
+      amount: 100,
+      currency: 'USD',
+      score: 0
+    },
+    {
+      id: 2,
+      userId: 'def456',
+      amount: 120,
+      currency: 'EUR',
+      score: 6
+    },
+    {
+      id: 3,
+      userId: 'ghi789',
+      amount: 80,
+      currency: 'GBP',
+      score: 8
+    }
+  ];
+
+  onModalShow() {
+    if(this.selectedOffer !== undefined && this.selectedOffer.score !== undefined){
+      this.rating = this.selectedOffer.score;
+    }
   }
 }
